7ade0735af5f7105cfef5cc141f00ea479857fa3
Ben Unell <97351508+Bunell5090@users.noreply.github.com>
2024-07-08 18:35:26 -0500

    Updated Ruby Version file

--- app/controllers/coin_controller.rb
+++ app/controllers/coin_controller.rb
@@ -1,2 +1,15 @@
 class CoinController < ApplicationController
+
+ def index
+  coin = Coin.all.order(:id) 
+  render json: coin
+ end
+
+ def show
+  coin Coin.find_by(id: params[:id])
+  render json: coin
+ end
+
+ def create
+  
 end
